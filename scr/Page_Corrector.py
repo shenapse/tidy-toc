aa82b5dadcf2e9526d1b60fccf2468f88c249da2
shena <int0tfdxt104@gmail.com>
2022-08-24 00:20:17 +0900

    refactored. + Mediator class + Suggester class + Fill class + Correct class

--- scr/Page_Corrector.py
+++ scr/Page_Corrector.py
@@ -4,14 +4,14 @@ import re
 from dataclasses import dataclass
 from enum import Enum, IntEnum, auto
 from re import Pattern
-from typing import Optional
+from typing import Callable, Optional
 
 import click
 from rich import print
 from typing_extensions import Self
 
 from Text_Line import Paged_Text_Line
-from Text_Lines import Paged_Text_Lines
+from Text_Lines import Paged_Text_Lines, Texts_Printer
 
 
 class Option(Enum):
@@ -28,8 +28,7 @@ class Choice:
     number: int
 
 
-class Page_Interpreter:
-    _highlight: str = "magenta"
+class Mediator:
     _pat_digit: Pattern = re.compile("^[+-]?\\d+$")
     _pat_plus: Pattern = re.compile("^[+]\\d+$")
 
@@ -41,12 +40,15 @@ class Page_Interpreter:
 
         @classmethod
         def get_flag(cls, x: str) -> Self:
-            return cls.Plus if re.search(Page_Interpreter._pat_plus, x) else cls.Normal
+            return cls.Plus if re.search(Mediator._pat_plus, x) else cls.Normal
 
-    def __init__(self, private_options: list[str] = ["a", "f"], max_page: int = 10000) -> None:
-        self._public_options: list[str] = [opt.value for opt in Option if opt.value not in private_options]
+    def __init__(
+        self, public_options: list[str] = ["p", "r"], private_options: list[str] = ["f"], max_page: int = 10000
+    ) -> None:
+        self._public_options: list[str] = [opt.value for opt in Option if opt.value in public_options]
         self._private_options: list[str] = [opt.value for opt in Option if opt.value in private_options]
         self._max_page: int = max_page
+        self._printer = Texts_Printer()
 
     def _test_digit(self, text: str) -> bool:
         return re.search(self._pat_digit, text) is not None
@@ -71,9 +73,6 @@ class Page_Interpreter:
         # otherwise
         return None, flag_default
 
-    def _generate_color_block(self) -> tuple[str, str]:
-        return f"[{self._highlight}]", "[/]"
-
     def get_public_options(self) -> list[Option]:
         return [opt for opt in Option if opt.value in self._public_options]
 
@@ -81,17 +80,17 @@ class Page_Interpreter:
         return [opt for opt in Option if opt.value in self._private_options]
 
     def explain(self) -> None:
-        begin, end = self._generate_color_block()
+        begin, end = self._printer.generate_color_block()
         explain_options: str = ", ".join([f"{opt.value}: {begin}{opt.name}{end}" for opt in self.get_public_options()])
         explain_integer: str = f"enter {begin}Integer{end}"
         msg: str = ", ".join([explain_integer, explain_options])
         print(msg)
 
-    def interpret(self, user_input: int | str, flag: Page_Interpreter.Integer_Flag) -> Choice:
+    def interpret(self, user_input: int | str, flag: Mediator.Integer_Flag) -> Choice:
         """convert user-input integer via click into choice object."""
         if isinstance(user_input, int):
             # fill user input. flag tells us which option should be used.
-            option: Option = Option.Append if flag == Page_Interpreter.Integer_Flag.Plus else Option.Fill
+            option: Option = Option.Append if flag == Mediator.Integer_Flag.Plus else Option.Fill
             return Choice(option=option, number=user_input)
         # other options are simply converted back to option objects
         # their number property are not interesting
@@ -101,12 +100,17 @@ class Page_Interpreter:
         raise ValueError(f"Unknown option. {user_input}")
 
     def get_user_input(
-        self, allow_suggest: bool = True, default_value: int | str = Option.Pass.value
-    ) -> tuple[int | str, Page_Interpreter.Integer_Flag]:
+        self,
+        msg: str = "enter action. default =",
+        allow_suggest: bool = True,
+        default_value: int | str = Option.Pass.value,
+        show_msg: bool = True,
+    ) -> tuple[int | str, Mediator.Integer_Flag]:
         """ask user to enter some input that represents her choice."""
+        msg = msg if show_msg else ""
         while True:
-            ret: tuple[int | str | None, Page_Interpreter.Integer_Flag] = click.prompt(
-                text="enter action. default =", type=str | int, value_proc=self._convert_input, default=default_value
+            ret: tuple[int | str | None, Mediator.Integer_Flag] = click.prompt(
+                text=msg, type=str | int, value_proc=self._convert_input, default=default_value
             )
             inp, flag = ret
             if inp is None:
@@ -121,44 +125,33 @@ class Page_Interpreter:
 
 
 class Suggester:
-    def __init__(
-        self, inter: Page_Interpreter, lines_blank_page_number: Paged_Text_Lines, lines_ref: Paged_Text_Lines
-    ) -> None:
-        self._inter: Page_Interpreter = inter
-        self._lines: Paged_Text_Lines = lines_blank_page_number
+    def __init__(self, lines_ref: Paged_Text_Lines, default_value: str = Option.Pass.value) -> None:
         self._lines_ref: Paged_Text_Lines = lines_ref
+        self.default_value: str = default_value
 
-    def _get_neighoring_lines(self, line: Paged_Text_Line) -> list[Paged_Text_Line]:
-        """get neighboring two rows relative to input line object in original lines object.
-        this method is intended to be called triggered by choice == suggest"""
-        idx: int = self._lines_ref.search(line.idx)
-        if idx == -1:
-            raise Exception(f"{line} is not found in {self._lines_ref}.")
-        return [self._lines_ref[i] for i in [idx - 1, idx + 1] if 0 <= i < self._lines_ref.len()]
+    def suggest(self, line: Paged_Text_Line) -> str:
+        """suggest default page-number for the given line"""
+        arounds: Paged_Text_Lines = self._lines_ref.get_rows_around(line=line, radius=1)
+        if len(arounds) == 0:
+            return self.default_value
 
-    def suggest(self, line: Paged_Text_Line) -> Choice:
-        """search neighboring rows of input line and show them to user, and then ask input again.
-        this method is called triggered by choice==suggest, and can't be called successively.
-        """
-        neibs: list[Paged_Text_Line] = self._get_neighoring_lines(line)
-        if len(neibs) == 0:
-            print(f"no rows are found near {line}")
-            user_input, flag = self._inter.get_user_input(allow_suggest=False)
-            return self._inter.interpret(user_input=user_input, flag=flag)
-        # show these surrounding lines
-        print("it's between")
-        for neib in neibs:
-            neib.print()
         # set default value
-        default_value: int = 0
-        for neib in reversed(neibs):
+        default_value: str = self.default_value
+        for around in reversed(arounds):
             # prioritize the successor row here just for my preference
-            if neib.page_number is not None:
-                default_value = neib.page_number
+            if around.page_number is not None:
+                default_value = str(around.page_number)
                 break
-        # further suggestion is not allowed
-        user_input, flag = self._inter.get_user_input(allow_suggest=False, default_value=str(default_value))
-        return self._inter.interpret(user_input=user_input, flag=flag)
+        return default_value
+
+    def get_processed_suggestion(
+        self,
+        line: Paged_Text_Line,
+        processor: Callable[[str, Paged_Text_Line], str],
+        condition: Callable[[str, Paged_Text_Line], bool],
+    ) -> str:
+        suggested: str = self.suggest(line)
+        return processor(suggested, line) if condition(suggested, line) else suggested
 
 
 class Fill:
@@ -169,24 +162,23 @@ class Fill:
     ) -> None:
         self._lines: Paged_Text_Lines = lines_blank_page_number
         self._lines_ref: Paged_Text_Lines = lines_ref
-        self.inter: Page_Interpreter = Page_Interpreter(private_options=["f", "a"])
-        self.suggest: Suggester = Suggester(
-            inter=self.inter, lines_blank_page_number=lines_blank_page_number, lines_ref=lines_ref
-        )
+        self.mediator: Mediator = Mediator(public_options=["p", "r"], private_options=["f"])
+        self.suggest: Suggester = Suggester(lines_ref=lines_ref, default_value=Option.Pass.value)
+        self.printer = Texts_Printer()
 
     def get_filled_lines(self) -> Paged_Text_Lines:
         """interactively ask user to fill numbers in rows of missing page number. return the filled lines object."""
         new_lines: list[Paged_Text_Line] = []
         delete_idx: list[int] = []
-        if self._lines.len() > 0:
-            self.inter.explain()
-        for line in self._lines:
-            line.print()
-            user_input, flag = self.inter.get_user_input()
-            choice = self.inter.interpret(user_input=user_input, flag=flag)
+        if len(self._lines) > 0:
+            self.mediator.explain()
+        for i, line in enumerate(self._lines):
+            self.printer.print_around(self._lines_ref, row=line.idx, N_around=1)
+            user_input, flag = self.mediator.get_user_input(show_msg=(i == 0), default_value=self.suggest.suggest(line))
+            choice = self.mediator.interpret(user_input=user_input, flag=flag)
             # if suggest is chosen, ask user to re-input
-            if choice.option == Option.Suggest:
-                choice = self.suggest.suggest(line)
+            # if choice.option == Option.Suggest:
+            #     choice = self.suggest.suggest(line)
             match choice.option:
                 case Option.Pass:
                     continue
@@ -196,35 +188,44 @@ class Fill:
                 case Option.Remove:
                     delete_idx.append(line.idx)
                 case _:
-                    raise Exception(f"unknow choice type {choice.option}.")
-        return self._lines.exclude(delete_idx).overwrite(new_lines)
+                    raise Exception(f"unknown choice type {choice.option}.")
+        return self._lines_ref.exclude(delete_idx).overwrite(new_lines)
 
 
 class Correct:
     def __init__(
-        self, lines_strange_page_number: Paged_Text_Lines, lines_ref: Paged_Text_Lines, ignore: list[int] = []
+        self,
+        lines_strange_page_number: Paged_Text_Lines,
+        lines_ref: Paged_Text_Lines,
+        ignore: list[int] = [],
+        append_key: list[str] = ["chapter", "part", "section"],
     ) -> None:
         self._lines: Paged_Text_Lines = lines_strange_page_number
         self._lines_ref: Paged_Text_Lines = lines_ref
         self._ignore: list[int] = ignore
-        self.inter: Page_Interpreter = Page_Interpreter(private_options=["f", "a"])
-        self.suggest: Suggester = Suggester(
-            inter=self.inter, lines_blank_page_number=lines_strange_page_number, lines_ref=lines_ref
+        self.mediator: Mediator = Mediator(public_options=["p", "r"], private_options=["f", "a"])
+        self.suggest: Suggester = Suggester(lines_ref=lines_ref)
+        self.printer = Texts_Printer()
+        self.pat_append: Pattern = re.compile("|".join([f"({key})" for key in append_key]))
+
+    def _test_to_append(self, suggested: str, line: Paged_Text_Line) -> bool:
+        return re.search("^\\d+$", suggested) is not None and re.search(self.pat_append, line.text.lower()) is not None
+
+    def get_suggestion(self, line: Paged_Text_Line) -> str:
+        return self.suggest.get_processed_suggestion(
+            line, processor=lambda s, _: "+" + s, condition=self._test_to_append
         )
 
     def get_corrected_lines(self) -> Paged_Text_Lines:
         """interactively ask user to fill numbers in rows of missing page number. return the filled lines object."""
         new_lines: list[Paged_Text_Line] = []
         delete_idx: list[int] = []
-        if self._lines.len() > 0:
-            self.inter.explain()
-        for line in self._lines:
-            line.print()
-            user_input, flag = self.inter.get_user_input()
-            choice = self.inter.interpret(user_input=user_input, flag=flag)
-            # if suggest is chosen, ask user to re-input
-            if choice.option == Option.Suggest:
-                choice = self.suggest.suggest(line)
+        if len(self._lines) > 0:
+            self.mediator.explain()
+        for i, line in enumerate(self._lines):
+            self.printer.print_around(self._lines_ref, row=line.idx, N_around=1)
+            user_input, flag = self.mediator.get_user_input(show_msg=(i == 0), default_value=self.get_suggestion(line))
+            choice = self.mediator.interpret(user_input=user_input, flag=flag)
             match choice.option:
                 case Option.Pass:
                     continue
@@ -239,5 +240,5 @@ class Correct:
                 case Option.Remove:
                     delete_idx.append(line.idx)
                 case _:
-                    raise Exception(f"unknow choice type {choice.option}.")
-        return self._lines.exclude(delete_idx).overwrite(new_lines)
+                    raise Exception(f"unknown choice type {choice.option}.")
+        return self._lines_ref.exclude(delete_idx).overwrite(new_lines)
--- tests/page/test__page.py
+++ tests/page/test__page.py
@@ -5,11 +5,9 @@ sys.path.append(os.path.join(".", "scr"))
 from typing import TypeAlias
 
 import pytest
-from Page_Corrector import Page_Interpreter
+from Page_Corrector import Mediator
 
-# from scr.Page_Corrector import Page_Interpreter
-
-Inter: TypeAlias = Page_Interpreter
+# from scr.Page_Corrector import Mediator
 
 
 def get_alphabets() -> list[str]:
@@ -33,17 +31,18 @@ def data_test_digit() -> list[tuple[str, bool]]:
 
 
 @pytest.fixture
-def data_convert_input_ok() -> list[tuple[str, str | int, Inter.Integer_Flag]]:
+def data_convert_input_ok() -> list[tuple[str, str | int, Mediator.Integer_Flag]]:
     """assumes 's' options are allowed"""
-    P = Inter()
-    n = P.Integer_Flag.Normal
-    p = P.Integer_Flag.Plus
-    return [("10", 10, n), ("-1", -1, n), ("+9", 9, p)] + [(option, option, n) for option in P._public_options]
+    med = Mediator()
+    n = med.Integer_Flag.Normal
+    p = med.Integer_Flag.Plus
+    return [("10", 10, n), ("-1", -1, n), ("+9", 9, p)] + [(option, option, n) for option in med._public_options]
 
 
 @pytest.fixture
 def data_range_out() -> list[tuple[str, bool]]:
-    thr: int = Inter._max_page
+    med = Mediator()
+    thr: int = med._max_page
     return [
         (str(-thr - 1), False),
         (str(-thr), True),
@@ -53,11 +52,11 @@ def data_range_out() -> list[tuple[str, bool]]:
 
 
 @pytest.fixture
-def data_convert_input_ng() -> list[tuple[str, None, Inter.Integer_Flag]]:
+def data_convert_input_ng() -> list[tuple[str, None, Mediator.Integer_Flag]]:
     """assumes some options are allowed"""
-    P = Inter()
-    n = Inter.Integer_Flag.Normal
-    return [(c, None, n) for c in get_alphabets() if c not in P._public_options]
+    med = Mediator()
+    n = Mediator.Integer_Flag.Normal
+    return [(c, None, n) for c in get_alphabets() if c not in med._public_options]
 
 
 # -------------------------
@@ -66,14 +65,14 @@ def data_convert_input_ng() -> list[tuple[str, None, Inter.Integer_Flag]]:
 
 
 def test_test_digit(data_test_digit):
-    P = Inter()
+    P = Mediator()
     for inp, ans in data_test_digit:
         print(f"inp={inp}, ans={ans}")
         assert P._test_digit(inp) == ans
 
 
 def test_convert_input_ok(data_convert_input_ok):
-    P = Inter()
+    P = Mediator()
     for inp, ans_out, ans_flag in data_convert_input_ok:
         out, flag = P._convert_input(inp)
         print(f"inp={inp}, acutal out={out}")
@@ -82,14 +81,14 @@ def test_convert_input_ok(data_convert_input_ok):
 
 
 def test_range_out(data_range_out):
-    P = Inter()
+    P = Mediator()
     for inp, res in data_range_out:
         converted, _ = P._convert_input(inp)
         assert isinstance(converted, int) and P._test_range(converted) == res
 
 
 def test_convert_input_ng(data_convert_input_ng):
-    P = Inter()
+    P = Mediator()
     for inp, ans_out, ans_flag in data_convert_input_ng:
         out, flag = P._convert_input(inp)
         assert out == ans_out
